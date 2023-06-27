36ee590fc5c05aea0e87148a569b62887b34e48d
mertguney <guneeymert@outlook.com>
2023-06-27 12:39:05 +0300

    Email service created

---
+++ src/Core/IdentityServer.Application/Services/Abstractions/IMailService.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityServer.Application.Services.Abstractions;
+
+public interface IMailService
+{
+    Task<bool> SendAsync(string to, string subject, string body);
+    Task<bool> SendAsync(List<string> addresses, string subject, string body);
+    Task<bool> SendEmailConfirmationMailAsync(string to, string userId, string token);
+}
+
---
+++ src/Infrastructure/IdentityServer.Infrastructure/Services/MailService.cs
@@ -0,0 +1,99 @@
+﻿namespace IdentityServer.Infrastructure.Services;
+public class MailService : IMailService
+{
+    private readonly ILogger<MailService> _logger;
+
+    public MailService(ILogger<MailService> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<bool> SendAsync(string to, string subject, string body)
+    {
+        try
+        {
+            var smtpClient = GetSmtpClient();
+            var message = GetMailMessage(to, subject, body);
+            await smtpClient.SendMailAsync(message);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while mail sending.");
+            return false;
+        }
+    }
+
+    public async Task<bool> SendAsync(List<string> addresses, string subject, string body)
+    {
+        try
+        {
+            var smtpClient = GetSmtpClient();
+            var message = GetMailMessage(addresses, subject, body);
+            await smtpClient.SendMailAsync(message);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while mail sending.");
+            return false;
+        }
+    }
+
+    public async Task<bool> SendEmailConfirmationMailAsync(string to, string userId, string token)
+    {
+        return await SendAsync(to, "Email Confirmation", $"{userId}/{token}");
+    }
+
+    public async Task<bool> SendResetPasswordMailAsync(string to, string userId, string token)
+    {
+        return await SendAsync(to, "Reset Password", $"{userId}/{token}");
+    }
+
+    public async Task<bool> SendForgotPasswordMailAsync(string to, string userId, string token)
+    {
+        return await SendAsync(to, "Forgot Password", $"{userId}/{token}");
+    }
+
+    private static SmtpClient GetSmtpClient()
+    {
+        return new SmtpClient("host", 25)
+        {
+            EnableSsl = false,
+            UseDefaultCredentials = false,
+            Credentials = new NetworkCredential("username", "password")
+        };
+    }
+
+    private static MailMessage GetMailMessage(string to, string subject, string body)
+    {
+        MailMessage message = new()
+        {
+            Body = body,
+            Subject = subject,
+            IsBodyHtml = true,
+            From = new MailAddress("xxx@mail.com")
+        };
+        message.To.Add(new MailAddress(to));
+
+        return message;
+    }
+
+    private static MailMessage GetMailMessage(List<string> addresses, string subject, string body)
+    {
+        MailMessage message = new()
+        {
+            Body = body,
+            Subject = subject,
+            IsBodyHtml = true,
+            From = new MailAddress("xxx@mail.com")
+        };
+
+        foreach (var address in addresses)
+        {
+            message.To.Add(new MailAddress(address));
+        }
+
+        return message;
+    }
+}
