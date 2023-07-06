151439468471413de7965e968b8a07f1f2a4dab6
MertGuney <guneeymert@gmail.com>
2023-07-06 17:31:17 +0300

    Merge bug fixed

--- src/Core/IdentityServer.Application/Services/Abstractions/IUserService.cs
+++ src/Core/IdentityServer.Application/Services/Abstractions/IUserService.cs
@@ -10,6 +10,10 @@ public interface IUserService
 
     Task<ResponseModel<NoContentModel>> UpdateAsync(User User);
 
+    Task<ResponseModel<NoContentModel>> VerifyEmailAsync(User user);
+
+    Task<ResponseModel<NoContentModel>> AddToRoleAsync(User user, string role);
+
     Task<ResponseModel<NoContentModel>> CreateAsync(User user, string password);
 
     Task<ResponseModel<NoContentModel>> CreateLoginAsync(User user, UserLoginInfo loginInfo);
