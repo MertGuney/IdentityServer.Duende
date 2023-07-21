acdb4a6272e11fa01062135a3eb12a5daf4ca3f1
MertGuney <guneeymert@gmail.com>
2023-07-21 20:20:55 +0300

    Failure and Role constants added #4

---
+++ src/Core/IdentityServer.Application/Common/Constants/RoleContants.cs
@@ -0,0 +1,8 @@
+﻿namespace IdentityServer.Application.Common.Constants;
+
+public class RoleContants
+{
+    public const string Manager = "Manager";
+    public const string Customer = "Customer";
+    public const string Administrator = "Administrator";
+}
---
+++ src/Shared/IdentityServer.Shared/Constants/FailureConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace IdentityServer.Shared.Constants;
+
+public class FailureConstants
+{
+    public const int ApplicationError = 1000;
+
+    public const int ValidationError = 1100;
+
+    public const int UserNotFound = 1205;
+
+    public const int SendEmail = 1210;
+}
